9ce538ef41727fca8634ab7cca99621c7a52e8c6
Adi Sharma <70466673+iadisharma@users.noreply.github.com>
2021-04-05 21:35:08 +0530

    Add files via upload

---
+++ holorectangleprob.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+   int row, col;
+   cout<<"Enter Rows \n";
+   cin>>row;
+   cout<<"Enter Columns \n";
+   cin>>col;
+    
+   for(int i=1; i<=row ; i++){
+
+       for(int j=1; j<=col; j++){
+           if (i==1 || i==row){
+               cout<<"*";
+           }
+           else if (j==1 || j==col){
+               cout<<"*";
+           }
+           else
+           cout<<" ";
+       }
+      
+       cout<<endl;
+
+   }
+
+}
